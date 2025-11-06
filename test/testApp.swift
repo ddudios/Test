58658de8c88ec962d5529e4de63f5a7f7fdbbcc1
ddudi <ddudios@naver.com>
2025-11-06 10:15:20 +0900

    Initial commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Suji Jang on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
